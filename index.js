3a921a621c6e3285feca0c458585f96ed456f63d
Teri <teri.eich@gmail.com>
2019-01-02 02:33:51 +0000

    refactored

--- index.js
+++ index.js
@@ -16,20 +16,24 @@ class Transaction {
     this.account = account;
   }
 
+  commit() {
+    this.account.balance += this.value;
+  }
+
 }
 
 class Deposit extends Transaction {
 
-  commit() {
-    this.account.balance += this.amount;
+  get value() {
+    return this.amount;
   }
 
 }
 
 class Withdrawal extends Transaction {
 
-  commit() {
-    this.account.balance -= this.amount;
+  get value() {
+    return -this.amount;
   }
 
 }
@@ -41,16 +45,16 @@ class Withdrawal extends Transaction {
 
 const myAccount = new Account('snow-patrol');
 
-t1 = new Withdrawal(50.25, myAccount);
+console.log(`-- ${myAccount.username}'s account --`);
+console.log('Starting Balance: ', myAccount.balance);
+
+const t1 = new Withdrawal(50.25, myAccount);
 t1.commit();
-console.log('Transaction 1:', t1);
 
-t2 = new Withdrawal(9.99, myAccount);
+const t2 = new Withdrawal(9.99, myAccount);
 t2.commit();
-console.log('Transaction 2:', t2);
 
-t3 = new Deposit(120.00, myAccount);
+const t3 = new Deposit(120.00, myAccount);
 t3.commit();
-console.log('Transaction 3:', t3);
 
-console.log('Balance:', myAccount.balance);
+console.log('Ending Balance: ', myAccount.balance);
